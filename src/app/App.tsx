af8b57c2e55f34da11f9c7305a2769fbb74b0df2
LeonardoFlorentino <leonardo.ff.21@gmail.com>
2021-10-29 01:42:09 -0300

    Change fetchData logic

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -11,15 +11,11 @@ const App = () => {
   const fetchData = (nameLogin: string, profile: string) => {
     if (profile === 'mainUser' || Object.entries(user).length === 0) {
       setUserName(nameLogin)
-      fetch(`https://api.github.com/users/${nameLogin}`)
-        .then((res) => res.json())
-        .then((value) => setUser(value))
     }
-    else if (profile === 'subordinateUser') {
-      fetch(`https://api.github.com/users/${nameLogin}`)
-        .then((res) => res.json())
-        .then((value) => setUser(value))
-    }
+
+    fetch(`https://api.github.com/users/${nameLogin}`)
+      .then((res) => res.json())
+      .then((value) => setUser(value))
   }
 
   return (
