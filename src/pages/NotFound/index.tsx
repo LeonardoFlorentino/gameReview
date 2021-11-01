38f0720547b2438d8861983efc1207d5a328d644
LeonardoFlorentino <leonardo.ff.21@gmail.com>
2021-11-01 18:09:45 -0300

    Delete the page notFound

--- src/pages/NotFound/index.tsx
+++
@@ -1,9 +0,0 @@
-import notFoundGit from '../../assets/img/profile-first-pr-darg.svg'
-
-export const NotFound = () => {
-    return (
-    <div>
-        <img src={notFoundGit} alt="" />
-    </div>
-    )
-}
